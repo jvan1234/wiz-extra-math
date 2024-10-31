e5250b8ce4500e83a8639e1d9a69c16b8b73d5a9
jvan1234 <72505202+jvan1234@users.noreply.github.com>
2024-10-31 16:41:53 -0400

    Update pxt.json, main.blocks, main.ts, custom.ts

---
+++ custom.ts
@@ -0,0 +1,40 @@
+
+/**
+* Use this file to define custom functions and blocks.
+* Read more at https://arcade.makecode.com/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100
+//% color=#1e83e8
+//% icon="\uf201"
+namespace custom {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function foo(n: number, s: string, e: MyEnum): void {
+        // Add code here
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
